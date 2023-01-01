d352cc192785dacaa8de94001dbe5857112f88a9
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-01 20:52:09 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Philipp Sanktjohanser on 01.01.23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
